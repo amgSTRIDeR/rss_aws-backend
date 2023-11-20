610c5df52e0d4519a9f4068d9635678dc658a45d
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-11-20 18:18:00 +0400

    feat: lambda and gateway

--- cdk-lambda/lib/cdk-lambda-stack.ts
+++ cdk-lambda/lib/cdk-lambda-stack.ts
@@ -1,19 +1,32 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkLambdaQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const getProductsHandler = new lambda.Function(this, 'ProductsHandler', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'getProducts.handler',
     });
-
-    const topic = new sns.Topic(this, 'CdkLambdaTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+  
+    const getProductHandler = new lambda.Function(this, 'ProductHandler', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'getProduct.handler',
+    });
+  
+    const apiGateway = new apigw.RestApi(this, 'Endpoint', {});
+  
+    const productsResource = apiGateway.root.addResource('products');
+    
+    productsResource.addMethod('GET', new apigw.LambdaIntegration(getProductsHandler));
+  
+    const productResource = apiGateway.root.addResource('{id}');
+    productResource.addMethod('GET', new apigw.LambdaIntegration(getProductHandler));
   }
+
 }
