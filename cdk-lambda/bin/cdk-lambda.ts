0ecbb5ba97b70565d4e0b34f0378ea572eb624bb
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-11-20 13:58:51 +0400

    refactor: change project to simple-app init

---
+++ cdk-lambda/bin/cdk-lambda.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { CdkLambdaStack } from '../lib/cdk-lambda-stack';
+
+const app = new cdk.App();
+new CdkLambdaStack(app, 'CdkLambdaStack');
