74a5c730a2c62c98a1d1b4ce76d47670509a2fc7
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-12-05 16:49:11 +0400

    feat: implement importFileParser

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -5,9 +5,8 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
-import { EventType} from 'aws-cdk-lib/aws-s3';
+import { EventType } from 'aws-cdk-lib/aws-s3';
 import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
-
 
 export const BUCKET_NAME = 'import-bucket-st';
 
@@ -20,9 +19,15 @@ export class ImportServiceStack extends cdk.Stack {
       roleName: 'import-service-role',
     });
 
-    iAmRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'));
-    iAmRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('CloudWatchLogsFullAccess'));
-    iAmRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AWSLambda_FullAccess'));
+    iAmRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess')
+    );
+    iAmRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('CloudWatchLogsFullAccess')
+    );
+    iAmRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AWSLambda_FullAccess')
+    );
 
     const importBucket = new s3.Bucket(this, 'ImportBucketSt', {
       bucketName: BUCKET_NAME,
@@ -41,31 +46,37 @@ export class ImportServiceStack extends cdk.Stack {
       ],
     });
 
+    const importProductsFileLambda = new lambda.Function(
+      this,
+      'importProductsFileLambdaSt',
+      {
+        functionName: 'import-products-file',
+        runtime: lambda.Runtime.NODEJS_18_X,
+        handler: 'importProductsFile.handler',
+        code: lambda.Code.fromAsset('lambda'),
+        role: iAmRole,
+        environment: {
+          BUCKET: BUCKET_NAME,
+          REGION: 'eu-west-1',
+        },
+      }
+    );
 
-
-    const importProductsFileLambda = new lambda.Function(this, 'importProductsFileLambdaSt', {
-      functionName: 'import-products-file',
-      runtime: lambda.Runtime.NODEJS_18_X,
-      handler: 'importProductsFile.handler',
-      code: lambda.Code.fromAsset('lambda'),
-      role: iAmRole,
-      environment: {
-        BUCKET: BUCKET_NAME,
-        REGION: 'eu-west-1',
-      },
-    });
-
-    const importFileParserLambda = new lambda.Function(this, 'importFileParserLambdaSt', {
-      functionName: 'import-file-parser',
-      runtime: lambda.Runtime.NODEJS_18_X,
-      handler: 'importFileParser.handler',
-      code: lambda.Code.fromAsset('lambda'),
-      role: iAmRole,
-      environment: {
-        BUCKET: BUCKET_NAME,
-        REGION: 'eu-west-1',
-      },
-    });
+    const importFileParserLambda = new lambda.Function(
+      this,
+      'importFileParserLambdaSt',
+      {
+        functionName: 'import-file-parser',
+        runtime: lambda.Runtime.NODEJS_18_X,
+        handler: 'importFileParser.handler',
+        code: lambda.Code.fromAsset('lambda'),
+        role: iAmRole,
+        environment: {
+          BUCKET: BUCKET_NAME,
+          REGION: 'eu-west-1',
+        },
+      }
+    );
 
     importBucket.grantReadWrite(importProductsFileLambda);
     importBucket.grantReadWrite(importFileParserLambda);
@@ -90,10 +101,8 @@ export class ImportServiceStack extends cdk.Stack {
     importBucket.addEventNotification(
       EventType.OBJECT_CREATED,
       new LambdaDestination(importFileParserLambda),
-      { prefix: 'uploaded' },
+      { prefix: 'uploaded/' },
     );
 
-
-    
   }
 }
