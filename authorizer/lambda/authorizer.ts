9532d673d13d6a1c61354286bbd5c6d6b03288cf
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-12-18 17:38:50 +0400

    refactor: authorizer

--- authorizer/lambda/authorizer.ts
+++ authorizer/lambda/authorizer.ts
@@ -3,78 +3,32 @@ import {
   APIGatewayAuthorizerResult,
 } from 'aws-lambda';
 
-export async function handler(event: APIGatewayTokenAuthorizerEvent) {
-  console.log(event);
-  const token = event.authorizationToken;
+export async function handler(
+  event: APIGatewayTokenAuthorizerEvent,
+  context: any,
+  callback: any
+) {
+  const authorizationToken = event.authorizationToken;
+  console.log('authorizationToken', authorizationToken);
 
-  console.log(token);
-
-  if (!token) {
-    console.log('401: Can\'t find authorization token');
-    return {
-      statusCode: 401,
-      body: JSON.stringify({
-        message: "Can't find authorization token",
-      }),
-    };
+  if (!authorizationToken) {
+    callback('Unauthorized');
   }
 
   try {
-    const encodedCreds = token.split(' ')[1];
-    const buff = Buffer.from(encodedCreds, 'base64')
+    const encodedCreds = authorizationToken.split(' ')[1];
+    const buff = Buffer.from(encodedCreds, 'base64');
     const plainCreds = buff.toString('utf-8').split(':');
-    console.log(`plainCreds: ${plainCreds}`)
+    console.log(`plainCreds: ${plainCreds}`);
     const username = plainCreds[0];
     const password = plainCreds[1];
-
-    if (!username || !password ) {
-      console.log('403: Something wrong with the token');
-      return {
-        statusCode: 403,
-        body: JSON.stringify({
-          message: "User not found",
-        }),
-      };
-    }
-
-    console.log(`username: ${username}, password: ${password}`);
-
     const storedUserPassword = process.env[username];
 
-    console.log(storedUserPassword)
-
-    if (!storedUserPassword) {
-      console.log('403: User not found');
-      return {
-        statusCode: 403,
-        body: JSON.stringify({
-          message: "User not found",
-        }),
-      };
-    }
-
-
-    if (password === storedUserPassword) {
-      const policy = generatePolicy(username, event.methodArn, 'Allow');
-      console.log('200: Access granted');
-      return policy;
-    } else {
-      console.log('403: Access denied - invalid token');
-      return {
-        statusCode: 403,
-        body: JSON.stringify({
-          message: "Access denied - invalid token",
-        }),
-      };
-    }
-  } catch (err) {
-    console.log('500: Internal server error');
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: "Internal server error",
-      }),
-    };
+    password === storedUserPassword
+      ? callback(null, generatePolicy(username, event.methodArn, 'Allow'))
+      : callback(null, generatePolicy(username, event.methodArn, 'Deny'));
+  } catch (err: any) {
+    callback(`Unauthorized: ${err.message}`);
   }
 }
 
