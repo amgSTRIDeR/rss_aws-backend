1292baf7bf093ea94aca95a88557fdee04c5b05d
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-12-18 15:54:34 +0400

    fix: header name

--- authorizer/lambda/authorizer.ts
+++ authorizer/lambda/authorizer.ts
@@ -7,7 +7,10 @@ export async function handler(event: APIGatewayTokenAuthorizerEvent) {
   console.log(event);
   const token = event.authorizationToken;
 
+  console.log(token);
+
   if (!token) {
+    console.log('401: Can\'t find authorization token');
     return {
       statusCode: 401,
       body: JSON.stringify({
@@ -15,14 +18,24 @@ export async function handler(event: APIGatewayTokenAuthorizerEvent) {
       }),
     };
   }
-  console.log(token);
 
   try {
     const encodedCreds = token.split(' ')[1];
     const buff = Buffer.from(encodedCreds, 'base64')
     const plainCreds = buff.toString('utf-8').split(':');
+    console.log(`plainCreds: ${plainCreds}`)
     const username = plainCreds[0];
     const password = plainCreds[1];
+
+    if (!username || !password ) {
+      console.log('403: Something wrong with the token');
+      return {
+        statusCode: 403,
+        body: JSON.stringify({
+          message: "User not found",
+        }),
+      };
+    }
 
     console.log(`username: ${username}, password: ${password}`);
 
@@ -31,6 +44,7 @@ export async function handler(event: APIGatewayTokenAuthorizerEvent) {
     console.log(storedUserPassword)
 
     if (!storedUserPassword) {
+      console.log('403: User not found');
       return {
         statusCode: 403,
         body: JSON.stringify({
@@ -42,8 +56,10 @@ export async function handler(event: APIGatewayTokenAuthorizerEvent) {
 
     if (password === storedUserPassword) {
       const policy = generatePolicy(username, event.methodArn, 'Allow');
+      console.log('200: Access granted');
       return policy;
     } else {
+      console.log('403: Access denied - invalid token');
       return {
         statusCode: 403,
         body: JSON.stringify({
@@ -52,6 +68,7 @@ export async function handler(event: APIGatewayTokenAuthorizerEvent) {
       };
     }
   } catch (err) {
+    console.log('500: Internal server error');
     return {
       statusCode: 500,
       body: JSON.stringify({
